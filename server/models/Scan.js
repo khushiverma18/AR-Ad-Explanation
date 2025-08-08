c11a03bf6c16ae10545b4442fe6e1d71b545bca6
@khushiverma18 <147837115+khushiverma18@users.noreply.github.com>
2025-08-08 15:10:14 +0530

    Delete server directory

--- server/models/Scan.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const scanSchema = new mongoose.Schema({
-  campaignId: String,
-  timestamp: { type: Date, default: Date.now },
-  timeSpent: Number
-});
-
-export default mongoose.model('Scan', scanSchema);
--- server/routes/analyticsRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { recordScan, getSummary } from '../controllers/analyticsController.js';
-
-const router = express.Router();
-router.post('/record', recordScan);
-router.get('/summary', getSummary);
-export default router;
